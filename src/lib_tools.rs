a71efc020a827613bfa1cb6c615cd710dbfd46ab
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-10-11 01:37:26 +0530

    feat: case insensitive search logic

--- src/lib_tools.rs
+++ src/lib_tools.rs
@@ -1,8 +1,9 @@
 pub mod utils{
     pub fn search<'a>(content:&'a str,query:&str)->Vec<&'a str>{
+        let query=query.to_lowercase();
         let mut ans=Vec::new();
         for line in content.lines(){
-            if line.contains(query){
+            if line.to_lowercase().contains(&query){
                 ans.push(line);
             }
         }
